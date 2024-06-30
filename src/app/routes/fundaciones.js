e88cc189dcff74af2283d8f77b089d5109fa174c
tizianocarranza <tizianocarranza0910@gmail.com>
2024-06-30 16:25:37 -0300

    Monolitica first stage

--- src/app/routes/fundaciones.js
+++ src/app/routes/fundaciones.js
@@ -1,19 +1,58 @@
 const express = require("express");
 const router = express.Router();
-const { getFundaciones, getFundacionesPorEtiqueta, getFundacion, createFundacion, updateFundacion, deleteFundacion } = require("../controllers/fundaciones");
-const { authUser } = require("../middleware/authUser");
-const { upload, uploadFileToGridFS } = require("../middleware/upload");
+const mongoose = require("mongoose");
 
-router.get("/", getFundaciones);
+const fundacionesSchema = new mongoose.Schema({
+    logo: String,
+    titulo: String,
+    horario: String,
+    direccion: String,
+    telefono: String,
+    sitioWeb: String,
+    mapaBoton: String,
+    email: {
+        type: String,
+        unique: true, // Este campo debe ser único
+        required: true
+    },
+    mapa: String,
+    descripcion: String,
+    tituloEtiquetas: [String],
+    //password: {
+    //    type: String,
+    //    required: true
+    //},
+},
+{
+    timestamps: true,
+    versionKey: false
+});
 
-router.get("/etiqueta", getFundacionesPorEtiqueta);
+const fundacionModel = mongoose.model('Fundaciones', fundacionesSchema, "Fundaciones");
 
-router.get("/:id", getFundacion);
 
-router.post("/", authUser, upload.single("logo"), uploadFileToGridFS, createFundacion);
+router.get("/etiqueta", async (req, res, next) => {
+    try {
+        const etiqueta = req.query.etiqueta;
 
-router.put("/:id", authUser, upload.single("logo"), uploadFileToGridFS, updateFundacion);
+        const fundaciones = await fundacionModel.find({
+            tituloEtiquetas: { $in: etiqueta }
+        });
+        res.send({ list: fundaciones });
+    } catch (error) {
+        next(new Error("Error al obtener las fundaciones por etiqueta: " + error.message));
+    }
+}
+);
 
-router.delete("/:id", authUser, deleteFundacion);
+router.get("/", async (req, res, next) => {
+    try {
+        const listAll = await fundacionModel.find({})
+        res.send({ list: listAll });
+    }
+    catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router;
